e8e23eb605dcca1c0745716bbeb0bc95cf7d38f4
UGUIS8020 <shibuyamasahiko@gmail.com>
2024-04-30 01:37:10 +0900

    大袋

--- chatbot_engine.py
+++ chatbot_engine.py
@@ -1,15 +1,21 @@
 from langchain.chat_models import ChatOpenAI
 import langchain
+import langchain.indexes
 from langchain.memory import ChatMessageHistory
 from langchain.schema import HumanMessage
-
+from langchain.document_loaders import DirectoryLoader
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.indexes.vectorstore import VectorStoreIndexWrapper
 
 langchain.verbose = True
 
-def chat(message: str,history: ChatMessageHistory) -> str:
+def create_index() ->VectorStoreIndexWrapper:
+    loader = DirectoryLoader("./src/", globa="**/*.py")
+    return VectorstoreIndexCreator().from_loaders([loader])
+   
+
+def chat(message: str,history: ChatMessageHistory, index: VectorStoreIndexWrapper) -> str:
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
 
-    messages = history.messages
-    messages.append(HumanMessage(content=message))
+    return index.query(question=message, llm=llm)
 
-    return llm(messages).content
